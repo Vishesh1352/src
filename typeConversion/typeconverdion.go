1e3e150517062e399d46e635406774e4fec01416
vishesh <dhandhalavishesh@gmail.com>
2022-06-13 12:55:00 +0530

    Learning typeconversion

---
+++ typeConversion/typeconverdion.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type hotdog int
+
+var a int
+var b hotdog
+
+func main() {
+	b = 43
+	a = 42
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+	a = int(b)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+}
